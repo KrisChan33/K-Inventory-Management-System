530b07af38b7e53f76a23725a3ffd89844684d3d
Kristian Jay Tadeo <Kristianjaytadeo11@gmail.com>
2024-10-16 11:41:15 +0800

    fix json error by adding a cast function

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,7 +46,6 @@ class User extends Authenticatable implements HasAvatar
     //     'email_verified_at' => 'datetime',
     //     'password' => 'hashed',
     //     'custom_fields' =>'array'
-
     // ];
 
     protected function casts(): array
