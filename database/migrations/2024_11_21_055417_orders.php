aefbfbc9313becdd4bc1a8c46f3ad3d95932aec8
Kristian Jay Tadeo <Kristianjaytadeo11@gmail.com>
2024-11-29 16:09:25 +0800

    make some changes

--- database/migrations/2024_11_21_055417_orders.php
+++ database/migrations/2024_11_21_055417_orders.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use Filament\Tables\Columns\Summarizers\Count;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,13 +13,16 @@ return new class extends Migration
      */
     public function up(): void
     {
+
         Schema::create('orders', function (Blueprint $table) {
+        $user = User::where('user_id');
+
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-            $table->integer('quantity');
             $table->string('order_number');
-            $table->decimal('total', 10, 2);
-            $table->string('status'); // pending, processing, completed, cancelled
+            $table->decimal('total', 10, 2)->nullable();
+            $table->string('status')->default('Pending'); // pending, processing, completed, cancelled
+            $table->text('message_for_seller')->nullable();
             $table->timestamps();
         });
     }
